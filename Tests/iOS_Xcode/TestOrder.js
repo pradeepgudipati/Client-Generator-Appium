a932500e591f5f25d12235a8bc411aab3f367b08
Tejasree Marthy <tmarthy@innominds.com>
2018-02-01 15:42:14 +0530

    Uncommented test order over ACL module

--- Tests/iOS_Xcode/TestOrder.js
+++ Tests/iOS_Xcode/TestOrder.js
@@ -3,16 +3,16 @@
 exports.tests = [
 
 	// Tests for the User module
-	// 'User/Create.Positive.Test.js',
-	// 'User/Login.Positive.Test.js',
-	// 'User/Login.Negative.Test.js',
-	// 'User/Logout.Positive.Test.js',
-	// 'User/Query.Negative.Test.js',
-	// 'User/Show.Positive.Test.js',
-	// 'User/Show.Negative.Test.js',
-	// 'User/Update.Positive.Test.js',
-	// 'User/Update.Negative.Test.js',
-	// 'User/Remove.Positive.Test.js',
+	'User/Create.Positive.Test.js',
+	'User/Login.Positive.Test.js',
+	'User/Login.Negative.Test.js',
+	'User/Logout.Positive.Test.js',
+	'User/Query.Negative.Test.js',
+	'User/Show.Positive.Test.js',
+	'User/Show.Negative.Test.js',
+	'User/Update.Positive.Test.js',
+	'User/Update.Negative.Test.js',
+	'User/Remove.Positive.Test.js',
 
 	// Tests for the ACL module
 	'ACL/Create.Positive.test.js',
@@ -27,19 +27,19 @@ exports.tests = [
 	'ACL/Delete.Positive.test.js',
 	
 	// Tests for the Chat modul
-	// 'Chat/Create.Positive.Test.js',
-	// 'Chat/Create.Negative.Test.js',
-	// 'Chat/Query.Positive.Test.js',
+	'Chat/Create.Positive.Test.js',
+	'Chat/Create.Negative.Test.js',
+	'Chat/Query.Positive.Test.js',
 
-	// // Tests for the Place module
-	// 'Place/Create.Positive.Test.js',
-	// 'Place/Search.Positive.Test.js',
-	// 'Place/Search.Negative.Test.js',
-	// 'Place/Update.Positive.Test.js',
-	// 'Place/Delete.Positive.Test.js',
+	// Tests for the Place module
+	'Place/Create.Positive.Test.js',
+	'Place/Search.Positive.Test.js',
+	'Place/Search.Negative.Test.js',
+	'Place/Update.Positive.Test.js',
+	'Place/Delete.Positive.Test.js',
 
-	// // Tests for the Checkin module
-	// 'Checkin/Create.Positive.test.js',
-	// 'Checkin/Query.Positive.test.js'
+	// Tests for the Checkin module
+	'Checkin/Create.Positive.test.js',
+	'Checkin/Query.Positive.test.js'
 
 ]
