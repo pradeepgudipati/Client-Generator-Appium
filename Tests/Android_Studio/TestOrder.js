fe628b8c8cc0debf946623f0ecc3a1a32f12a9f6
Harika Annam <annamhari6@gmail.com>
2018-02-19 15:04:29 +0530

    Modified testorder file

--- Tests/Android_Studio/TestOrder.js
+++ Tests/Android_Studio/TestOrder.js
@@ -1,31 +1,24 @@
 'use strict';
 
 exports.tests = [
+
+
+	// Tests for the ACL module
+	'ACL/Create.Positive.Test.js',
+	'ACL/Show.Positive.Test.js',
+
+	//Tests for the User module
 	'User/Create.User.Positive.Test',
-// 	'User/Create.User.Positive.Test',
-//     'User/Create.User.Negative.Test.js',
-//     'User/Login.Positive.Test.js',
-//     'User/Login.Negative.Test.js',
-//     'User/Logout.Positive.Test.js',
-//     'User/User.Query.Positive.Test',
-//     'User/Show.Positive.Test.js',
-//     'User/Show.Negative.Test.js',
-//     'User/User.Update.Positive.Test.js',
-//     'User/User.Update.Negative.Test.js',
-//     'User/Remove.Negative.Test.js',
-//     'User/Remove.Positive.Test.js'
-
-	// // Tests for the ACL module
-	// 'ACL/Create.Positive.Test.js',
-	// 'ACL/Show.Positive.Test.js',
-
-	// Tests for the User module
-	// 'User/Create.Positive.Test.js',
-	// 'User/Login.Positive.Test.js',
-	// 'User/Login.Negative.Test.js',
-	// 'User/Logout.Positive.Test.js',
-	// 'User/Show.Positive.Test.js',
-	// 'User/Show.Negative.Test.js',
-	// 'User/Update.Positive.Test.js',
-	// 'User/Remove.Positive.Test.js'
+    'User/Create.User.Negative.Test.js',
+    'User/Login.Positive.Test.js',
+    'User/Login.Negative.Test.js',
+    'User/Logout.Positive.Test.js',
+    'User/User.Query.Positive.Test',
+    'User/Show.Positive.Test.js',
+    'User/Show.Negative.Test.js',
+    'User/User.Update.Positive.Test.js',
+    'User/User.Update.Negative.Test.js',
+    'User/Remove.Negative.Test.js',
+    'User/Remove.Positive.Test.js'
+	
 ]
