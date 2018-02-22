0cd49ec32615b0058346054d077759ab933b7b4f
pradeep_gudipati <pradeepgudipati@gmail.com>
2018-02-22 12:30:24 +0530

    Beautify Files

--- Tests/iOS_Xcode/Checkin/Delete.Positive.test.js
+++ Tests/iOS_Xcode/Checkin/Delete.Positive.test.js
@@ -1,56 +1,51 @@
 'use strict';
 
 const driver = global.driver,
-				webdriver = global.webdriver,
-				user = require(`${global.projRoot}/Config/data_config.js`).user,
-				place = require(`${global.projRoot}/Config/data_config.js`).tempPlace;
+	webdriver = global.webdriver,
+	user = require(`${global.projRoot}/Config/data_config.js`).user,
+	place = require(`${global.projRoot}/Config/data_config.js`).tempPlace;
 
 describe('Checkin Query Delete - Positive', () => {
-				before(() => {
-								return driver
-												.elementById('Users')
-												.click()
-												.elementById('Login User')
-												.click()
-												.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
-												.sendKeys(user.username)
-												.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
-												.sendKeys(user.password)
-												.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
-												.click()
-												.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.elementById('Axway')
-												.click()
-												.elementById('Checkins')
-												.click()
-												.waitForElementById('Query Checkin', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.waitForElementById(`Checked in to ${place.name}`, webdriver.asserters.isDisplayed, 10000);
-				});
+	before(() => {
+		return driver
+			.elementById('Users')
+			.click()
+			.elementById('Login User')
+			.click()
+			.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
+			.sendKeys(user.username)
+			.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
+			.sendKeys(user.password)
+			.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
+			.click()
+			.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.elementById('Axway')
+			.click()
+			.elementById('Checkins')
+			.click()
+			.waitForElementById('Query Checkin', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.waitForElementById(`Checked in to ${place.name}`, webdriver.asserters.isDisplayed, 10000);
+	});
 
-				after(() => {
-								return driver.resetApp();
-				});
+	after(() => {
+		return driver.resetApp();
+	});
 
-				it('Get Details and  Delete Checkin', () => {
-								return driver
-												.elementById(`Checked in to ${place.name}`)
-												.click()
-												.waitForElementById('Delete', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.waitForElementById('Ok', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.waitForElementByXPath('//XCUIElementTypeStaticText[2]', webdriver.asserters.isDisplayed, 10000)
-												.getAttribute('value')
-												.then(text => {
-																text
-																				.should
-																				.include('code = 200');
-																text
-																				.should
-																				.include('"method_name" = deleteCheckin');
-
-												});
-				});
+	it('Get Details and  Delete Checkin', () => {
+		return driver
+			.elementById(`Checked in to ${place.name}`)
+			.click()
+			.waitForElementById('Delete', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.waitForElementById('Ok', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.waitForElementByXPath('//XCUIElementTypeStaticText[2]', webdriver.asserters.isDisplayed, 10000)
+			.getAttribute('value')
+			.then(text => {
+				text.should.include('code = 200');
+				text.should.include('"method_name" = deleteCheckin');
+			});
+	});
 });
--- Tests/iOS_Xcode/Checkin/Query.Positive.test.js
+++ Tests/iOS_Xcode/Checkin/Query.Positive.test.js
@@ -1,54 +1,48 @@
 'use strict';
 
 const driver = global.driver,
-				webdriver = global.webdriver,
-				user = require(`${global.projRoot}/Config/data_config.js`).user,
-				place = require(`${global.projRoot}/Config/data_config.js`).tempPlace;
+	webdriver = global.webdriver,
+	user = require(`${global.projRoot}/Config/data_config.js`).user,
+	place = require(`${global.projRoot}/Config/data_config.js`).tempPlace;
 
 describe('Checkin Query - Positive', () => {
-				before(() => {
-								return driver
-												.elementById('Users')
-												.click()
-												.elementById('Login User')
-												.click()
-												.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
-												.sendKeys(user.username)
-												.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
-												.sendKeys(user.password)
-												.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
-												.click()
-												.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.elementById('Axway')
-												.click()
-												.elementById('Checkins')
-												.click()
-												.waitForElementById('Query Checkin', webdriver.asserters.isDisplayed, 10000)
-												.click()
-												.waitForElementById(`Checked in to ${place.name}`, webdriver.asserters.isDisplayed, 10000);
-				});
+	before(() => {
+		return driver
+			.elementById('Users')
+			.click()
+			.elementById('Login User')
+			.click()
+			.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
+			.sendKeys(user.username)
+			.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
+			.sendKeys(user.password)
+			.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
+			.click()
+			.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.elementById('Axway')
+			.click()
+			.elementById('Checkins')
+			.click()
+			.waitForElementById('Query Checkin', webdriver.asserters.isDisplayed, 10000)
+			.click()
+			.waitForElementById(`Checked in to ${place.name}`, webdriver.asserters.isDisplayed, 10000);
+	});
 
-				after(() => {
-								return driver.resetApp();
-				});
+	after(() => {
+		return driver.resetApp();
+	});
 
-				it('Get Checkin Details', () => {
-								return driver
-												.elementById(`Checked in to ${place.name}`)
-												.click()
-												.waitForElementByClassName('XCUIElementTypeTextView', webdriver.asserters.isDisplayed, 10000)
-												.getAttribute('value')
-												.then(text => {
-																text
-																				.should
-																				.include('code = 200');
-																text
-																				.should
-																				.include('"method_name" = showCheckins');
-																text
-																				.should
-																				.include(`message = "Checked in to ${place.name}"`);
-												});
-				});
+	it('Get Checkin Details', () => {
+		return driver
+			.elementById(`Checked in to ${place.name}`)
+			.click()
+			.waitForElementByClassName('XCUIElementTypeTextView', webdriver.asserters.isDisplayed, 10000)
+			.getAttribute('value')
+			.then(text => {
+				text.should.include('code = 200');
+				text.should.include('"method_name" = showCheckins');
+				text.should.include(`message = "Checked in to ${place.name}"`);
+			});
+	});
 });
