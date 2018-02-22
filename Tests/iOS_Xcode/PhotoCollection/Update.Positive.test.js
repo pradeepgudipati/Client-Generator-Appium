ea9e2bfa0c20cd6fad56a03df04a44c8d77d76bf
Tejasree Marthy <tmarthy@innominds.com>
2018-02-22 16:05:28 +0530

    Beautify - create and update files

--- Tests/iOS_Xcode/PhotoCollection/Update.Positive.test.js
+++ Tests/iOS_Xcode/PhotoCollection/Update.Positive.test.js
@@ -4,26 +4,27 @@ const
 
     driver = global.driver,
     webdriver = global.webdriver,
+    // creating instance to get data placed in data config file
     user = require(`${global.projRoot}/Config/data_config.js`).user,
     photocollection = require(`${global.projRoot}/Config/data_config.js`).photocollection
 
-    describe('Update - Photo Collection - Positive',() => {
-    
-        before( () => {
-            return driver
+describe('Update - Photo Collection - Positive', () => {
+
+    before(() => {
+        return driver
             .elementById('Users')
-			.click()
-			.elementById('Login User')
-			.click()
-			.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
-			.sendKeys(user.username)
-			.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
-			.sendKeys(user.password)
-			.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
-			.click()
-			.waitForElementById('OK', webdriver.asserters.isDisplayed, 5000)
-			.click()
-			.elementById('Axway')
+            .click()
+            .elementById('Login User')
+            .click()
+            .elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
+            .sendKeys(user.username)
+            .elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
+            .sendKeys(user.password)
+            .elementByXPath('//XCUIElementTypeButton[@name="Login"]')
+            .click()
+            .waitForElementById('OK', webdriver.asserters.isDisplayed, 5000)
+            .click()
+            .elementById('Axway')
             .click()
             .elementById('Photo Collections')
             .click()
@@ -31,47 +32,46 @@ const
             .click()
             .waitForElementById(photocollection.name, webdriver.asserters.isDisplayed, 5000)
             .isDisplayed().should.become(true);
-        });
+    });
 
-        after(() => {
-            return driver.resetApp();
-        });
+    after(() => {
+        return driver.resetApp();
+    });
 
-        it('Open the Photo Collection', () => {
-            return driver
-                .elementById(photocollection.name)
-                .click()
-                .elementById('Update')
-                .isDisplayed().should.become(true);
-        });
+    it('Open the Photo Collection', () => {
+        return driver
+            .elementById(photocollection.name) // binding static information to input fields
+            .click()
+            .elementById('Update')
+            .isDisplayed().should.become(true);
+    });
 
-        it('Update photo colection name', () => {
+    it('Update photo colection name', () => {
 
-            return driver
+        return driver
             .elementById('Update')
             .click()
             .elementById(photocollection.name)
-            .clear()
-            .sendKeys(photocollection.updatedName)
+            .clear() // will clear the text filed data
+            .sendKeys(photocollection.updatedName) // enters new/updated photo collection name
             .elementById(photocollection.updatedName)
             .isDisplayed().should.become(true);
 
-        });
-    
-        it('Update',() => {
+    });
 
-            return driver
-               .elementById('Update')
-               .click()
-               .waitForElementByXPath('//XCUIElementTypeStaticText[2]', webdriver.asserters.isDisplayed, 10000)
-			   .getAttribute('name')
-               .then(text => {
-				text.should.include('code = 200');
+    it('Update', () => {
+
+        return driver
+            .elementById('Update') // will update the selected photo subcollection
+            .click()
+            .waitForElementByXPath('//XCUIElementTypeStaticText[2]', webdriver.asserters.isDisplayed, 10000)
+            .getAttribute('name')
+            .then(text => {
+                text.should.include('code = 200');
                 text.should.include('"method_name" = updateCollection');
                 text.should.include(`name = "${photocollection.updatedName}"`);
             })
 
-        });
-
     });
 
+});
