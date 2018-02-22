a72e43ef813b5ffb639db1a104db746694e31d0e
Tejasree Marthy <tmarthy@innominds.com>
2018-02-22 16:05:58 +0530

    Beautify - Show and Remove files

--- Tests/iOS_Xcode/PhotoCollection/Remove.Positive.test.js
+++ Tests/iOS_Xcode/PhotoCollection/Remove.Positive.test.js
@@ -4,26 +4,27 @@ const
 
     driver = global.driver,
     webdriver = global.webdriver,
+    // creating instance to get data placed in data config file
     user = require(`${global.projRoot}/Config/data_config.js`).user,
     photocollection = require(`${global.projRoot}/Config/data_config.js`).photocollection;
 
-    describe('Remove - Photo Collection - Positive',() => {
-    
-        before( () => {
-            return driver
+describe('Remove - Photo Collection - Positive', () => {
+
+    before(() => {
+        return driver
             .elementById('Users')
-			.click()
-			.elementById('Login User')
-			.click()
-			.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
-			.sendKeys(user.username)
-			.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
-			.sendKeys(user.password)
-			.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
-			.click()
-			.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
-			.click()
-			.elementById('Axway')
+            .click()
+            .elementById('Login User')
+            .click()
+            .elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
+            .sendKeys(user.username)
+            .elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
+            .sendKeys(user.password)
+            .elementByXPath('//XCUIElementTypeButton[@name="Login"]')
+            .click()
+            .waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
+            .click()
+            .elementById('Axway')
             .click()
             .elementById('Photo Collections')
             .click()
@@ -31,16 +32,16 @@ const
             .click()
             .waitForElementById(photocollection.updatedName, webdriver.asserters.isDisplayed, 5000)
             .click();
-        });
+    });
 
-        after(() => {
-            return driver.resetApp();
-        });
+    after(() => {
+        return driver.resetApp();
+    });
 
-        it('Remove photo colection', () => {
+    it('Remove photo colection', () => {
 
-            return driver
-            .elementById('Remove')
+        return driver
+            .elementById('Remove') // will remove the selected photo collection
             .click()
             .elementById('Delete')
             .elementById('Ok')
@@ -50,9 +51,8 @@ const
             .then(text => {
                 text.should.include('code = 200');
                 text.should.include('"method_name" = deleteCollection');
-        });
+            });
 
     });
 
-    });
-
+});
--- Tests/iOS_Xcode/PhotoCollection/ShowSubCollection.Positive.test.js
+++ Tests/iOS_Xcode/PhotoCollection/ShowSubCollection.Positive.test.js
@@ -4,26 +4,27 @@ const
 
     driver = global.driver,
     webdriver = global.webdriver,
+    // creating instance to get data placed in data config file
     user = require(`${global.projRoot}/Config/data_config.js`).user,
     photocollection = require(`${global.projRoot}/Config/data_config.js`).photocollection
 
-    describe('ShowSubCollection - Photo Collection - Positive',() => {
-    
-        before( () => {
-            return driver
+describe('ShowSubCollection - Photo Collection - Positive', () => {
+
+    before(() => {
+        return driver
             .elementById('Users')
-			.click()
-			.elementById('Login User')
-			.click()
-			.elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
-			.sendKeys(user.username)
-			.elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
-			.sendKeys(user.password)
-			.elementByXPath('//XCUIElementTypeButton[@name="Login"]')
-			.click()
-			.waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
-			.click()
-			.elementById('Axway')
+            .click()
+            .elementById('Login User')
+            .click()
+            .elementByXPath('//XCUIElementTypeTextField[@value="Username"]')
+            .sendKeys(user.username)
+            .elementByXPath('//XCUIElementTypeSecureTextField[@value="Password"]')
+            .sendKeys(user.password)
+            .elementByXPath('//XCUIElementTypeButton[@name="Login"]')
+            .click()
+            .waitForElementById('OK', webdriver.asserters.isDisplayed, 10000)
+            .click()
+            .elementById('Axway')
             .click()
             .elementById('Photo Collections')
             .click()
@@ -31,32 +32,31 @@ const
             .click()
             .waitForElementById(photocollection.name, webdriver.asserters.isDisplayed, 10000)
             .isDisplayed().should.become(true);
-        });
+    });
 
-        after(() => {
-            return driver.resetApp();
-        });
-        it('Open the Photo Collection', () => {
-            return driver
-                .elementById(photocollection.name)
-                .click()
-                .elementById('Show SubCollection')
-                .isDisplayed().should.become(true);
-        });
-
-        it('Show SubCollection', () => {
-
-            return driver
+    after(() => {
+        return driver.resetApp();
+    });
+    it('Open the Photo Collection', () => {
+        return driver
+            .elementById(photocollection.name) // binding static information to input fields to check the photo collection
+            .click()
             .elementById('Show SubCollection')
+            .isDisplayed().should.become(true);
+    });
+
+    it('Show SubCollection', () => {
+
+        return driver
+            .elementById('Show SubCollection') // will remove the selected photo subcollection
             .click()
             .waitForElementByXPath('//XCUIElementTypeStaticText[2]', webdriver.asserters.isDisplayed, 10000)
             .getAttribute('name')
             .then(text => {
                 text.should.include('code = 200');
                 text.should.include('"method_name" = showCollectionSubcollections');
-                
-            });
-        });
 
+            });
     });
 
+});
