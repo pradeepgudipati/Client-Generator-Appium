bc215ff2dfdfb7f04879622e0199fdd4a2378ce9
Harika Annam <annamhari6@gmail.com>
2018-04-26 00:44:34 +0530

    Custom objects: Remove objects positive and negative cases (#22)
    * Custom objects: Remove objects positive and negative cases
    
    * Modified remove object cases
    
    * Added validation check for method name
    
    * Validation checks added and modified code

--- Config/data_config.js
+++ Config/data_config.js
@@ -6,6 +6,14 @@ exports.user = {
 	lastName: 'Ministrator',
 	email: 'admin@appcelerator.com'
 };
+exports.tempUser = {
+	username: 'wluu',
+	password: 'MonkeyLord!',
+	firstName: 'Wilson',
+	lastName: 'Luu',
+	email: 'wluu@appcelerator.com',
+	secondEmail: 'wluu@axway.com'
+};
 exports.newUserDetails = {
 	username: 'wluu2',
 	password: 'MonkeyLord!',
@@ -35,16 +43,8 @@ exports.tempPlace = {
 	latitude: '53.643126',
 	longitude: '-1.777756'
 };
-exports.photocollection = {
-	name: 'Axway Test Photo Collection',
-	updatedName: 'Test Axway Test Photo Collection'
-};
 exports.acl = {
-	name: 'Test_ACL',
-	readerName: 'Teja Sree',
-	writerName: 'Teja Sree',
-	updateReaderName: 'Ad Ministrator',
-	updateWiterName: 'Ad Ministrator'
+	name: 'Appcelerator Test ACL'
 };
 exports.customObject = {
 	className: 'Test',
@@ -54,5 +54,4 @@ exports.customObject = {
 	propertyValueUpdate: 'TestName1',
 	queryId: '5a68aece5bbb6a027502691a',
 	loginQueryId: '5a68adbd93e737027c026eec'
-
 };
